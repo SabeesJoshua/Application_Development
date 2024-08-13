ae9f1a5ce8baed03c121705b40212da44b92234f
SabeesJoshua <152067689+SabeesJoshua@users.noreply.github.com>
2024-08-13 11:14:28 +0530

    Delete src/main/java/com/example/backendlol/backend directory

--- src/main/java/com/example/backendlol/backend/repository/TimeOffRequestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backendlol.backend.repository;
-
-import com.example.backendlol.backend.model.TimeOffRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface TimeOffRequestRepository extends JpaRepository<TimeOffRequest, Long> {
-    List<TimeOffRequest> findByTeamLeadId(Long teamLeadId);
-}
